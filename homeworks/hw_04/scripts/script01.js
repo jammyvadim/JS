b9d96b84fadfd6f2002bdc4c3b122ac961141d01
jammyvadim <jammy.vadim@gmail.com>
2025-10-23 21:57:11 +0300

    commit_2025-10-23

---
+++ homeworks/hw_04/scripts/script01.js
@@ -0,0 +1,13 @@
+console.log('#I2XsG6f')
+console.log('– створити функцію, яка обчислює та повертає площу прямокутника зі сторонами  а і б')
+
+function get_square_rectangle_num(rectangle_side_a_num, rectangle_side_b_num){
+    let result = rectangle_side_a_num * rectangle_side_b_num
+    return result
+}
+let a = 10
+let b = 20
+
+console.log('a=' + a)
+console.log('b=' + b)
+console.log('get_square_rectangle_num(a,b)=' + get_square_rectangle_num(a,b))
---
+++ homeworks/hw_04/scripts/script02.js
@@ -0,0 +1,11 @@
+console.log('#ETGAxbEn8l')
+console.log('– створити функцію, яка обчислює та повертає площу кола з радіусом r')
+
+function get_square_circle_num(circle_radius_num){
+    let result = Math.PI * circle_radius_num ** 2
+    return result
+}
+let r = 10
+
+console.log('r=' + r)
+console.log('get_square_circle_num(r)=' + get_square_circle_num(r))
