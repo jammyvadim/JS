4076ce079089ff4ce7c1f3d707b42995af19c637
jammyvadim <jammy.vadim@gmail.com>
2025-10-23 22:28:57 +0300

    commit_2025-10-23

---
+++ homeworks/hw_04/scripts/script05.js
@@ -0,0 +1,12 @@
+console.log('#59g0IsA')
+console.log('– створити функцію, яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент')
+
+function doc_write_text_as_par(text_str){
+// variant 1:
+//     document.write('<p>' + text_str + '</p>')
+// variant 2:
+    document.write(`<p>${text_str}</p>`)
+}
+let text='abc'
+document.write('text="' + text +'"')
+doc_write_text_as_par(text)
