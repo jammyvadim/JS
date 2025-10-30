95b1cc6de19493b0eaf28956135f85b0ecdd7d1a
jammyvadim <jammy.vadim@gmail.com>
2025-10-30 17:43:07 +0200

    commit-2025-10-30

--- homeworks/hw_06/scripts/script02.js
+++ homeworks/hw_06/scripts/script02.js
@@ -1,12 +1,18 @@
-console.log('#ETGAxbEn8l')
-console.log('– створити функцію, яка обчислює та повертає площу кола з радіусом r')
+console.log('#8lld9HMxXWB')
+console.log('– Перевести до великого регістру наступні стрінгові значення')
+console.log('"hello world", "lorem ipsum", "javascript is cool"')
 
-// function get_square_circle_num(circle_radius_num){
-//     let result = Math.PI * circle_radius_num ** 2
-//     return result
-// }
-let r = 10
-let get_square_circle_num = (r) => Math.PI * r ** 2
 
-console.log('r=' + r)
-console.log('площа кола =' + get_square_circle_num(r))
+let sentence1 = 'hello world'
+let sentence2 = 'lorem ipsum'
+let sentence3 = 'javascript is cool'
+
+let get_upper_str = (str) => str.toUpperCase()
+
+console.log(get_upper_str(sentence1))
+console.log(get_upper_str(sentence2))
+console.log(get_upper_str(sentence3))
+
+document.write('<p>' + get_upper_str(sentence1) +'</p>')
+document.write('<p>' + get_upper_str(sentence2) +'</p>')
+document.write('<p>' + get_upper_str(sentence3) +'</p>')
