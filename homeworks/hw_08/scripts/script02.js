f0da7b8415bc402e0b3cc58eef032712466c7b9c
jammyvadim <jammy.vadim@gmail.com>
2025-11-18 16:16:23 +0200

    commit_2025-11-18

---
+++ homeworks/hw_08/scripts/script02.js
@@ -0,0 +1,30 @@
+console.log('#AiN5CoUQ')
+console.log('Створити функцію, яка робить глибоку копію об’єкта.')
+console.log('Додати перевірки на undefined, null, NaN.')
+console.log('Подумати і реалізувати логіку, в якій кінцевий об’єкт буде мати функції, які в нього були до цього моменту.')
+
+function cloner(obj) {
+    if (obj) {
+        let functions = [];
+        for (const key in obj) {
+            if (typeof obj[key] === 'function') {
+                const functionClone = obj[key].bind({});
+                functions.push({functionClone, key});
+            }
+        }
+        const cloneObj = JSON.parse(JSON.stringify(obj));
+        for (const func of functions) {
+            cloneObj[func.key] = func.functionClone;
+        }
+
+        console.log(cloneObj);
+        return cloneObj
+
+    }
+
+    throw new Error('!!!!!');
+
+}
+
+const clone = cloner({id: 123, name: 'asdsad', greeting() {console.log('hello')}, foo() {console.log('bar')}});
+clone.foo();
