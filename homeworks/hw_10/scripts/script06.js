5788bf897de7085f7d411c6d9cdabfaa1b3d66bb
jammyvadim <jammy.vadim@gmail.com>
2025-11-28 10:00:59 +0200

    commit_2025-11-28

--- homeworks/hw_10/scripts/script06.js
+++ homeworks/hw_10/scripts/script06.js
@@ -1,6 +1,16 @@
-console.log('#')
-console.log('')
-console.log('')
-console.log('')
-console.log('')
-console.log('')
+console.log('#Jg0gPO00')
+console.log('створити конвертор ваги з кг в фунти. дані заповнюються через інпут.')
+console.log('При введенні даних обрахунок стається миттєво, без натискань додаткових кнопок')
+
+const form_kilo_to_pound = document.getElementById('form_kilo_to_pound')
+const input_kilo = form_kilo_to_pound.input_kilo
+const input_pound = form_kilo_to_pound.input_pound
+const index_kilo_to_pound = 2.0462262
+
+input_kilo.oninput = function (){
+    input_pound.value = input_kilo.value * index_kilo_to_pound
+}
+
+input_pound.oninput = function (){
+    input_kilo.value = input_pound.value / index_kilo_to_pound
+}
--- homeworks/hw_10/scripts/script07.js
+++ homeworks/hw_10/scripts/script07.js
@@ -1,6 +1,33 @@
-console.log('#')
-console.log('')
-console.log('')
-console.log('')
-console.log('')
-console.log('')
+console.log('#RbQGnH5DuC')
+console.log('В localStorage зберігаються масиви.')
+console.log('Вам потрібно зробити функцію, які дістає потрібний вам масив з localStorage та додає в нього об’єкт')
+console.log('сигнатура функції –')
+console.log('addToLocalStorage(arrayName:string,objToAdd:any{}):void')
+
+let arr_local_storage=[]
+let form_main = document.getElementById('id_form_main')
+let ul_local_storage = document.getElementById('ul_local_storage')
+
+for (let i = 0; i < localStorage.length; i++) {
+     const li_localStorage_item = document.createElement('li')
+     li_localStorage_item.classList.add('li_localStorage_item')
+     arr_local_storage[i] = localStorage.key(i)
+     li_localStorage_item.innerText = arr_local_storage[i]
+     ul_local_storage.append(li_localStorage_item)
+}
+
+form_main.onsubmit=function(){
+    let obj={
+        customer_name: form_main.input_customer_name.value,
+        customer_city: form_main.input_customer_city.value,
+        customer_phone: form_main.input_customer_phone.value
+    }
+    let arr = []
+    for (const item of arr_local_storage) {
+        if (item===form_main.input_arrayName.value){
+            arr = JSON.parse(localStorage.getItem(form_main.input_arrayName.value))
+        }
+    }
+    arr.push([obj])
+    localStorage.setItem(form_main.input_arrayName.value,JSON.stringify(arr))
+}
